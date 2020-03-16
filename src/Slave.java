15e7065164b3bc133faa275cc37e3f6eb28d2ce8
MichelYABA <46414213+MichelYABA@users.noreply.github.com>
2020-03-16 12:44:42 +0100

    Update Slave.java

--- src/Slave.java
+++ src/Slave.java
@@ -12,46 +12,15 @@ import java.net.SocketException;
 public class Slave implements Runnable{
     private  final Socket socket;
     
-    Slave(Socket s){
+    Slave(Socket s, File f){
         this.socket = s;
         
     }
 
     @Override
     public void run() {
-    	InputStream is;
-        try {
-        	/*Preparation des entrees*/
-        	is = socket.getInputStream();
-        	InputStreamReader isr = new InputStreamReader(is);
-            BufferedReader input = new BufferedReader(isr);
-            
-            //Preparation des sorties
-            OutputStream os = socket.getOutputStream();
-            PrintWriter pw = new PrintWriter(os, true);
-            
-            for(;;){
-				String req;
-				while((req=input.readLine())!=null)
-					//String rep = ;
-					pw.println("Size = "+req.length());
-			}
-				
-           
-         }
-        catch (SocketException e) {
-            System.out.println(e);
-        }
-        catch (IOException e) {
-            System.out.println(e);
-        }
-
-        try { 
-            socket.close(); 
-        }
-        catch (IOException e) {
-            System.out.println(e);
-        }
+    	
+         
     }
 }
    
